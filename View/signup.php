b92af4e00f0f4cb330f98ddb73ae49e51a12e08b
Khaled M.Fathy <eng.khaled.93@hotmail.com>
2015-12-24 01:55:05 +0200

    Sign Up Form

---
+++ View/signup.php
@@ -0,0 +1,9 @@
+<html !DOCTYPE=html>
+	<form method="post">
+		<input type ="text" name="firstname" placeholder ="First Name" required>
+		<input type ="text" name="lname" placeholder="Last Name" required>
+		<input type ="email" name="email" placeholder="Email Address" required>
+		<input type ="password" name="password" placeholder="Password" required>
+		<input type ="password" name="passwordconfirmation" placeholder="Confirm Password" required>
+	</form>
+</html>
