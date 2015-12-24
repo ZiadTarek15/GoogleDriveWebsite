e971e1294856da84d9ff5e933a215f0ee61220ed
Ziad Tarek <zinedineziad15@gmail.com>
2015-12-25 01:36:44 +0200

    yarab2

---
+++ View/GradProject.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Grad_Projects</title>
+</head>
+<body>
+	<form action="gp.php">
+  ProjectName: <br>
+  <input type="text" name="projname" >
+  <br>
+  Year:<br>
+  <input type="text" name="year" >
+  <br>
+  Description:<br>
+  <input type="text" name="description" >
+  <br>
+  Doctor: <br>
+  <input type="text" name="doctor" >
+  <br>
+  TeamMembers:<br>
+  <input type="text" name="names" >
+  <br>
+  Membersnos:<br>
+  <input type="text" name="nos" >
+  <br>
+  Membersmails:<br>
+  <input type="text" name="emails" >
+  <br><br>
+
+</form>
+</body>
+</html>
