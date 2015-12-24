f0c2071fbfc30b5854c9198c9d8ffd1101806571
Khaled M.Fathy <eng.khaled.93@hotmail.com>
2015-12-24 19:31:42 +0200

    Finish Signin

--- View/signup.php
+++ View/signup.php
@@ -1,10 +1,10 @@
 <html !DOCTYPE=html>
 	<form method="post" action="../Controller/doSignUp.php">
-		<input type ="text" name="fname" placeholder ="First Name" required>
-		<input type ="text" name="lname" placeholder="Last Name" required>
-		<input type ="email" name="email" placeholder="Email Address" required>
-		<input type ="password" name="password" placeholder="Password" required>
-		<input type ="password" name="passwordconfirmation" placeholder="Confirm Password" required>
+		<input type ="text" name="fname" placeholder ="First Name" required><br>
+		<input type ="text" name="lname" placeholder="Last Name" required><br>
+		<input type ="email" name="email" placeholder="Email Address" required><br>
+		<input type ="password" name="password" placeholder="Password" required><br>
+		<input type ="password" name="passwordconfirmation" placeholder="Confirm Password" required><br>
 
 		<button type="submit">Sign Up!!</button>
 	</form>
