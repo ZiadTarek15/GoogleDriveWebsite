79714cda91c30411d532a80a0999f04f08d7f362
Khaled M.Fathy <eng.khaled.93@hotmail.com>
2015-12-25 19:29:28 +0200

    Add Background

--- View/signin.php
+++ View/signin.php
@@ -1,8 +1,21 @@
 <html !DOCTYPE=html>
+<head>
+	<title>Academic Content</title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<style type="text/css">
+	body{
+		background-image: url("../images/Website-Design-Background.png");
+		background-size: cover;
+		background-position: bottom;
+	}
+	</style>
+</head>
+<body>
 	<form method="post" action="../Controller/doSignIn.php">
 		<input type ="text" name="email" placeholder ="Email Address" required><br>
 		<input type ="password" name="password" placeholder="Password" required><br>
 
 		<button type="submit">Sign In!!</button>
 	</form>
+	</body>
 </html>
