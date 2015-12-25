61ecbcdf0009b8577710656f18642d22fd1dfa0f
tahagoal <tahagoal@gmail.com>
2015-12-26 01:23:08 +0200

    finish back end

--- Controller/dep_controller.php
+++ Controller/dep_controller.php
@@ -36,4 +36,31 @@ function get_years($dep_name)
 	}
 	return $data;
 }
+
+function this_id($year, $class, $name)
+{
+	$query = "SELECT * FROM `department` WHERE year = '$year' and class = '$class' and dep_name = '$name' ";
+	$data = mysqli_fetch_array($GLOBALS['conn']->query($query));
+
+	if ($GLOBALS['conn']->query($query) != false) {
+    	return $data[0];
+		} 
+		else 
+		{
+    echo "Error: " . $query . "<br>" . $GLOBALS['conn']->error;
+		}
+}
+function course_id($name,$code)
+{
+	$query = "SELECT * FROM `department` WHERE name = '$name' and course_code = '$code'";
+	$data = mysqli_fetch_array($GLOBALS['conn']->query($query));
+
+	if ($GLOBALS['conn']->query($query) != false) {
+    	return $data[0];
+		} 
+		else 
+		{
+    echo "Error: " . $query . "<br>" . $GLOBALS['conn']->error;
+		}
+}
 ?>
--- View/dep.php
+++ View/dep.php
@@ -1,28 +1,19 @@
 <?php
 include '../Controller/dep_controller.php';
 session_start();
-// $temp = $_SESSION['department_id']
-$temp = '15';
-$data = get_dep($temp);
+// $department_id = $_GET['department_id'];
+$department_id = '15';
+$data = get_dep($department_id);
 $dep_id = $data[0];
 $dep_name = $data[1];
 $dep_year = $data[2];
 $dep_class = $data[3];
 //kol entries al courses fe array wa7da, al course code w wrah al course name t7t hna v 
-$courses_data = get_courses($temp);
+$courses_data = get_courses($department_id);
 $dep_years = get_years($dep_name);
 ?>
 <html lang="en">
 <style type="text/css">
-  #left {
-    margin: 20px;
-    margin-left: 0px;
-    margin-top: 20px;
-    width: 150px;
-    height: 500px;
-    background-color: #ECECEC;
-    float: left;
-  }
 
   .fa 
   {
@@ -45,6 +36,11 @@ $dep_years = get_years($dep_name);
     margin: 0 50px 0 0;
         height: 500px;
     width: 500px;
+    font-family: 'Handlee', cursive;
+  }
+  ul p
+  {
+    font-family: 'Handlee', cursive;
   }
   .main{
     position: relative;
@@ -57,6 +53,12 @@ $dep_years = get_years($dep_name);
     position: relative;
     top:50px;
     left: 180px;
+    font-family: 'Handlee', cursive;
+  }
+  .headd
+  {
+    font-family: 'Handlee', cursive;
+    width: 70%;
   }
   .CN{
     font-size: 15px;
@@ -64,9 +66,57 @@ $dep_years = get_years($dep_name);
   }
   .course{
     position: relative;
-    left: 950px;
+    left: 300px;
     top: -150px;
+    width: 70%;
   }
+
+
+  #body {
+    margin-top:-20px;
+    background-color: rbga(0,0,0,0);
+    height: 625px;
+    font-family: 'Handlee', cursive;
+  }
+  #left {
+    width: 250px;
+    height: 547px;
+    background-color: #6C7A89;
+    float: left;
+    padding-top: -10px;
+    box-shadow: 0.5px 0px 1px #888888;
+    margin-top: 78px;
+    
+  }
+  #left #categorize {
+    margin-top: 100px;
+  }
+  .nav li {
+    text-align: center;
+  }
+  .nav a {
+    color: black;
+    font-size: 20px;
+  }
+  body {
+    font-family: 'Handlee', cursive;
+  }
+  .course label
+  {
+    font-family: 'Handlee', cursive;
+  }
+  .course button
+  {
+    font-family: 'Handlee', cursive;
+  }
+  .course input
+  {
+    font-family: 'Handlee', cursive;
+  }
+  .FList i{
+    text-align: center;
+  }
+  
 
 
 </style>
@@ -78,53 +128,66 @@ $dep_years = get_years($dep_name);
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <link href='https://fonts.googleapis.com/css?family=Handlee' rel='stylesheet' type='text/css'>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
-<h1>
+<h1 class = "headd">
 <?php
-echo $dep_name;
+echo $dep_name ." ". $dep_class ."th";
 ?>
 </h1>
 
-<div id="left" style="display:block;">
-    <ul class="nav nav-pills nav-stacked">
-       <?php for ($i=0; $i < 2; $i++) {  ?>
+<div id="body">
+  <div id="left" style="display:block;">
+    <ul class="nav">
+        <?php for ($i=0; $i < count($dep_years); $i++) {  ?>
         <li role="presentation">
-          <a href="#tags" aria-controls="year3" role="tab" data-toggle="tab"><?php echo $dep_years[$i]; ?></a>
+          <a href="#<?php echo $dep_years[$i]; ?>" aria-controls="<?php echo $dep_years[$i]; ?>" role="tab" data-toggle="tab"><?php echo $dep_years[$i]; ?></a>
         </li>
-        <?php } ?>
-      </ul>
+        <?php } ?>    
   </div>
-<div id="right">
-  <div class="tab-content" style="display: block;">
-    <div role="tabpanel" class="tab-pane active" id="number0">
-      <div class = "main">
-        <ul class = "FList">
-        <?php for ($i=0; $i < count($courses_data); $i=$i+2) {  ?>
-          <li>
-            <a href="#">
-              <i class="fa fa-folder-open fa-5x"><p class = "CN"><?php 
-              echo ( $courses_data[$i+1] . "(" . ($courses_data[$i]) . ")"); 
-              ?></p>
-            </i>
-            </a>
-          </li>
-          <?php } ?>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
+  <div id="right">
+        <div class="tab-content" style="display: block;">  
+          <?php for ($i=0; $i < count($dep_years); $i++) {  ?>
+            <div role="tabpanel" class="tab-pane" id="<?php echo $dep_years[$i]; ?>">
+              <div class = "main">
+                <ul class = "FList">
+                  <p><?php $this_id = this_id($dep_years[$i],'4',"cse"); ?></p>
+                   <?php $year_courses = get_courses($this_id)?>
+                   <?php for ($j=0; $j < count($year_courses); $j=$j+2) {  ?>
+                    <li>
+                      <?php $_SESSION['year_course'] = $dep_years[$i]; ?>
+                      <a href="course.php?course_code=<?php echo $year_courses[$j] ?>">
+                        <i class="fa fa-folder-open fa-5x"><p class = "CN"><?php 
+                        echo ( $year_courses[$j+1] . "(" . ($year_courses[$j]) . ")"); 
+                        ?></p>
+                      </i>
+                      </a>
+                    </li>
+                  <?php } ?>
+                </ul>
+              </div>
+            </div>
+          <?php } ?> 
+        </div>
+  </div> <!-- right !-->
+</div> <!-- body!-->
 
 <div class= "course">
-  <form method="post" action="../Controller/add_course.php">
-  <input type ="text" name="CC" placeholder ="Course code" required><br>
-  <input type ="text" name="CN" placeholder ="Course name" required><br>
-  <button type="submit">Add</button>
-  </form>
+  <form class="form-inline" method="post" action="../Controller/add_course.php">
+  <div class="form-group">
+    <label for="exampleInputName2">Course-Code</label>
+    <input type="text" name="CC" class="form-control" id="exampleInputName2" placeholder="course code">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail2">Course-Name</label>
+    <input type="text" name="CN" class="form-control" id="exampleInputEmail2" placeholder="course name">
+  </div>
+  <button type="submit" class="btn btn-default">Add</button>
+</form>
 </div>
 </body>
 </html>
