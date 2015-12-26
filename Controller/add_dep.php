587c2f185a1b97b846ac4bd2b3b2693543e8bcf9
Rana.Sallam <rana.sallam102@gmail.com>
2015-12-26 02:55:52 +0200

    addDep

---
+++ Controller/add_dep.php
@@ -0,0 +1,16 @@
+<?php
+include '../View/connection.php';
+$dep_id = $_POST['ID'];
+$dep_name = $_POST['DN'];
+$class = $_POST['DC'];
+$year = $_POST['DY'];
+
+$query = "INSERT INTO `drive_db`.`department`(`dep_id`,`dep_name`,`year`,`class`) 
+					  VALUES ('$dep_id','$dep_name','$year','$class')";
+if($GLOBALS['conn']->query($query)===TRUE)
+{
+	header('Location: ../View/dep.php');
+}else{
+	echo "ERROR ".$GLOBALS['conn']->error;
+}
+?>
